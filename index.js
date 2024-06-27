90e912c67beaeb8d48c67ae393589610d2313fd0
Akashx1550 <akash1630.be21@chitkara.edu.in>
2024-06-27 11:58:46 +0530

    Updated cors

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(express.json());
 app.use(cors({
     origin: 'https://trend-mart-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }));
 
 // Configure Cloudinary
