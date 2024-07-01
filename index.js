021f2d8cd3635187b64f39580f60028036e05309
Akashx1550 <akash1630.be21@chitkara.edu.in>
2024-07-01 17:37:15 +0530

    updated index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const atlasConnectionUri = process.env.MONGODB_URI;
 app.use(express.json());
 
 app.use(cors({
-    origin: ['https://trend-mart-frontend.vercel.app/', 'https://trend-mart-admin.vercel.app/'],
+    origin: ['https://trend-mart-frontend.vercel.app/', 'https://trend-mart-admin.vercel.app/', '/*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
