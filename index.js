156a3db0bfa9400e51d1a8c470b344ae18e3e7da
Akashx1550 <akash1630.be21@chitkara.edu.in>
2024-07-05 10:51:27 +0530

    Updated product.jsx

--- index.js
+++ index.js
@@ -225,7 +225,6 @@ app.post("/addtocart", fetchUser, async (req, res) => {
     let userData = await Users.findOne({ _id: req.user.id });
     userData.cartData[req.body.itemId] += 1;
     await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
-    res.send("Added");
 })
 
 //API to remove product from cartData
@@ -236,7 +235,6 @@ app.post("/removefromcart", fetchUser, async (req, res) => {
         userData.cartData[req.body.itemId] -= 1;
     }
     await Users.findOneAndUpdate({ _id: req.user.id }, { cartData: userData.cartData });
-    res.send("removed");
 })
 
 
